1b4b9dace5f5fb1eeec689e2998913121c4ac115
felipefinity <felipe-augusto1997@hotmail.com>
2020-08-13 19:54:31 -0300

    Controlling hit points

--- jogo.js
+++ jogo.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
@@ -14,6 +15,15 @@ function posicaoRandomica() {
     //remover o mosquito anterior (caso exista)
     if (document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove()
+
+        if (vidas > 3) {
+            alert('Game Over')
+        } else {
+            document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+
+            vidas++
+        }
+
     }
 
     var posicaoX = Math.floor(Math.random() * largura) - 90
@@ -32,6 +42,9 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function() {
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
 
